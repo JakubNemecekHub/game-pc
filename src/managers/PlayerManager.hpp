821bc0065968d4f7eda0039063ed9649f0dca6d2
Jakub Nemecek <jakubnemecek@hotmail.com>
2022-12-04 16:42:12 +0100

    Move Player's implementation into cpp file

--- src/managers/PlayerManager.hpp
+++ src/managers/PlayerManager.hpp
@@ -32,20 +32,9 @@ private:
 public:
 
     Player() {};
-    Player(AssetManager* assets)
-        : position{0, 0}, destination{0, 0}, is_walking{false}, speed{3}, scale{1}
-    {
-        const std::string id { "player" };
-        sprite_ = assets->get_sprite(id);
-    }
+    Player(AssetManager* assets);
 
-    void walk(int x, int y)
-    {
-        destination.x = x;
-        destination.y = y;
-        is_walking = true;
-        sprite_->set_animation("walk");
-    }
+    void walk(int x, int y);
 
 };
 
--- src/managers/src/PlayerManager.cpp
+++ src/managers/src/PlayerManager.cpp
@@ -1,6 +1,28 @@
 #include "../PlayerManager.hpp"
 
 
+/*
+    PlayerManager
+*/
+ Player::Player(AssetManager* assets)
+    : position{0, 0}, destination{0, 0}, is_walking{false}, speed{3}, scale{1}
+{
+    const std::string id { "player" };
+    sprite_ = assets->get_sprite(id);
+}
+
+
+void Player::walk(int x, int y)
+{
+    destination.x = x;
+    destination.y = y;
+    is_walking = true;
+    sprite_->set_animation("walk");
+}
+
+/*
+    PlayerManager
+*/
 PlayerManager::PlayerManager(LogManager* log)
     : log_{log} {}
 
